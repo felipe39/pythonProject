01ae53386ba7ef58bb5dea7fc04327fab231c113
Felipe falcão <felpssalvatore42@gmail.com>
2022-08-25 21:50:59 -0300

    Média

---
+++ ExercíciosCP2/treino.py
@@ -0,0 +1,10 @@
+# Pede 4 notas e calcula a média bimestral
+
+n1 = float(input('Entre com a primeira nota: '))
+n2 = float(input('Entre com a segunda nota: '))
+n3 = float(input('Entre com a terceira nota: '))
+n4 = float(input('Entre com a terceira nota: '))
+
+média = (n1+n2+n3+n4) / 4
+
+print(f'\nA média foi: {média}')
